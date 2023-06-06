f25685c165538477a14d06996a7a1a83fc64c865
Daniel <angelicodaniel93@gmail.com>
2023-06-06 15:13:44 +0200

    prova con smartphone

--- src/org/lessons/java/shop/Carrello.java
+++ src/org/lessons/java/shop/Carrello.java
@@ -9,7 +9,21 @@ public class Carrello {
         // SMARTPHONE
 //        Smartphone smartphone = new Smartphone("nokia", "indistruttibile", 20,0.2, 1234, 666);
 //        System.out.println(smartphone);
-
+        System.out.println("Inserisci nome smartphone");
+        String nome = scanner.nextLine();
+        System.out.println("Inserisci la descrizione:");
+        String descrizione = scanner.nextLine();
+        System.out.println("Inserisci il prezzo:");
+        double prezzo = scanner.nextDouble();
+        System.out.println("Inserisci l'IVA:");
+        double iva = scanner.nextDouble();
+        System.out.println("Inserisci l'IMEI:");
+        int imei = scanner.nextInt();
+        System.out.println("Inserisci la memoria:");
+        int memoria = scanner.nextInt();
+        scanner.nextLine();
+        Smartphone smartphone = new Smartphone(nome, descrizione,prezzo,iva,imei,memoria);
+        System.out.println(smartphone);
 
 
 
